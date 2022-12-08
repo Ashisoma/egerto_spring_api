c89ad91230c7ca508e8a489bed482b03a083a200
Ashisoma <okalionesmus@gmail.com>
2022-12-08 16:29:08 +0300

    depployment

--- src/main/java/com/example/egerton_spring_api/EgertonSpringApiApplication.java
+++ src/main/java/com/example/egerton_spring_api/EgertonSpringApiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+
 @EnableWebMvc
 //@RestController
 @RequestMapping
